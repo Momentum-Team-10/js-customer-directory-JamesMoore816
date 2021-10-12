b064a986da03d9981815dcbd009b77f634dfd0cd
James Moore <james816am@gmail.com>
2021-10-12 16:03:24 -0400

    Everything working as intended

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-let customerContainer = document.getElementById('customer-container')
+// creates a div inside body to hold all customer cards
+let container = document.createElement('div');
+document.body.appendChild(container)
+container.id = 'customer-container';
+
 
 // This function should create a new div for a customer
-// and a series of p tags inside that to display information
-
+// and a series of tags inside that to display information
 function createEntry(input) {
     // creates div element with id = first and last name
     let entry = document.createElement('div');
     document.getElementById('customer-container').appendChild(entry);
     entry.id = input.name.first + ' ' + input.name.last;
+    entry.classList = "customer-card";
+    // creates an <img> with class = thumbnail
+    let thumbnail = document.createElement('img');
+    entry.appendChild(thumbnail)
+    thumbnail.classList = 'thumbnail';
+    // attaches thumbnail link to <img>
+    thumbnail.src = input.picture.medium;
     // creates a <p> with class = name
-    let name = document.createElement('p');
-    entry.appendChild(name);
-    name.classList = 'name';
+    let fullname = document.createElement('p');
+    entry.appendChild(fullname);
+    fullname.classList = 'name';
     // capitalizes first letter of first and last name, puts those as string inside <p>
     let firstName = input.name.first.charAt(0).toUpperCase() + input.name.first.slice(1)
     let lastName = input.name.last.charAt(0).toUpperCase() + input.name.last.slice(1)
-    console.log(firstName);
-    console.log(lastName);
-    name.innerText = firstName + ' ' + lastName
+    fullname.innerText = firstName + ' ' + lastName
     // creates a <p> with class = email
     let email = document.createElement('p');
     entry.appendChild(email);
@@ -34,10 +42,27 @@ function createEntry(input) {
     let cityStateZip = document.createElement('p')
     entry.appendChild(cityStateZip);
     cityStateZip.classList = 'cityStateZip';
+    // Creates a variable for state abbrevation, then finds the state abbreviation
+    let stateAbbrev = '';
+    for (let item of usStates) {
+        if (item.name === input.location.state.toUpperCase()) {
+            stateAbbrev = item.abbreviation;
+        }
+    }
     // puts city, state, and postalcode info into cityStateZip <p>
-    cityStateZip.innerText = `${input.location.city}, ${input.location.state} ${input.location.postcode}`;
-    // NEED TO GO BACK UP AND CHANGE STATES TO ABBREVIATIONS
-    
+    cityStateZip.innerText = `${input.location.city}, ${stateAbbrev} ${input.location.postcode}`;
+    // creates a <p> with class = DOB
+    let birthdate = document.createElement('p')
+    entry.appendChild(birthdate);
+    birthdate.classList = 'DOB';
+    // puts birthdate into <p> using moment for formatting
+    birthdate.innerText = "DOB: " + moment(input.dob.date).format("MMM DD, YYYY");
+    // creates a <p> with class = memberSince
+    let memberSince = document.createElement('p')
+    entry.appendChild(memberSince);
+    memberSince.classList = 'memberSince';
+    // puts membership registration date in <p> using moment
+    memberSince.innerText = "Customer since: " + moment(input.registered.date).format("MMM DD, YYYY");
 }
 
 function runThis() {
@@ -46,10 +71,4 @@ function runThis() {
     }
 }
 
-createEntry(customers[0]);
 runThis();
-
-
-console.log(customers[1])
-let now = moment();
-console.log(now)
